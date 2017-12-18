cee42b3bbe619d7d0a0c41e7cf9307ed33dd18b8
kmoiseenko <polocatic@gmail.com>
2017-12-18 23:24:00 +0200

    Edit fuctions for each command.

--- index.js
+++ index.js
@@ -5,37 +5,41 @@ const API_LINK = 'https://api.nasa.gov/planetary/apod?api_key=rom93FHJOFb6TF4jSC
 const BOT_TOKEN = '508617689:AAEuLPKs-EhrjrYGnz60inYNZqakf6HJWc0';
 const BOT = new TelegramBot(BOT_TOKEN, {polling: true});
 
-// BOT.onText(/\/pic/, function (msg, match) {
-// 	let userId = msg.from.id;
-
-// 	getData(API_LINK).then(
-// 		response => {
-// 			console.log(response)
-// 			BOT.sendMessage(userId, response);
-// 		},
-// 		error => console.log(error)
-// 	);
-// });
-
-// BOT.onText(/\/desc/, function (msg, match) {
-// 	let userId = msg.from.id;
-
-// 	getData(API_LINK).then(
-// 		response => {
-// 			console.log(response)
-// 			BOT.sendMessage(userId, response);
-// 		},
-// 		error => console.log(error)
-// 	);
-// });
-
-BOT.onText(/\/all/, function (msg, match) {
+BOT.onText(/\/pic/, function (msg, match) {
 	let userId = msg.from.id;
 
 	getData(API_LINK).then(
 		response => {
-			console.log(response)
-			BOT.sendMessage(userId, response);
+			BOT.sendPhoto(userId, JSON.parse(response).url);
+		},
+		error => console.log(error)
+	);
+});
+
+BOT.onText(/\/desc/, function (msg, match) {
+	let userId = msg.from.id;
+
+	getData(API_LINK).then(
+		response => {
+			let parseResponse = JSON.parse(response);
+			let desc = '<b>' + parseResponse.title + '</b>' + '\n' + parseResponse.explanation;
+
+			BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
+		},
+		error => console.log(error)
+	);
+});
+
+BOT.onText(/\/all/, function (msg, match) {
+	let userId = msg.from.id;
+	
+	getData(API_LINK).then(
+		response => {
+			let parseResponse = JSON.parse(response);
+			let desc = '<b>' + parseResponse.title + '</b>' + '\n' + parseResponse.explanation;
+
+			BOT.sendPhoto(userId, parseResponse.url);
+			BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
 		},
 		error => console.log(error)
 	);

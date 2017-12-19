24edc6bf9a9ac7e0a0aad84cef03fa417a501562
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2017-12-19 18:16:17 +0200

    Add function that allow to store user id. Add function that allow to send messages to stored users every day

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // ------------------------------------------------------------
 let TelegramBot = require('node-telegram-bot-api');
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let jsonfile = require('jsonfile');
 
 
 // Variables
@@ -9,25 +10,81 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API_LINK = 'https://api.nasa.gov/planetary/apod?api_key=rom93FHJOFb6TF4jSC7USdH03jogPMtfg7qDHrMd';
 const BOT_TOKEN = '508617689:AAEuLPKs-EhrjrYGnz60inYNZqakf6HJWc0';
 const BOT = new TelegramBot(BOT_TOKEN, {polling: true});
+const DELAY = 21600000; // 6 hours
+// const DELAY = 60000; // 1 minute
+let usersData = 'users_data.json';
+let hours = 'hours.json';
+
+
+onInit();
 
 
 // Bot response
 // ------------------------------------------------------------
-BOT.onText(/\/pic/, function (msg, match) {
+BOT.onText(/\/pic/, (msg, match) => {
+	checkOutWithUsersFile(msg.from);
 	sendResponse(msg.from.id, 'pic');
 });
 
-BOT.onText(/\/desc/, function (msg, match) {
+BOT.onText(/\/desc/, (msg, match) => {
+	checkOutWithUsersFile(msg.from);
 	sendResponse(msg.from.id, 'desc');
 });
 
-BOT.onText(/\/all/, function (msg, match) {
+BOT.onText(/\/all/, (msg, match) => {
+	checkOutWithUsersFile(msg.from);
 	sendResponse(msg.from.id, 'all');
 });
 
 
 // Functions
 // ------------------------------------------------------------
+function onInit() {
+	jsonfile.readFile(hours, (err, obj) => {
+		if(err) {
+			console.log(err);
+		} else {
+			setInterval(() => {
+				obj["hours"] === new Date().getHours() ? sendResponseToAllUsers() : console.log("Not yet");
+			}, DELAY);
+		}
+	});
+}
+
+function sendResponseToAllUsers() {
+	jsonfile.readFile(usersData, (err, obj) => {
+		if(err) {
+			console.log(err);
+		} else {
+			for(let key in obj) {
+				sendResponse(parseInt(key), 'all');
+			}
+		}
+	});
+}
+
+function checkOutWithUsersFile(data) {
+	jsonfile.readFile(usersData, (err, obj) => {
+		if(err) {
+			console.log(err);
+		} else {
+			for(let key in obj) {
+				if(parseInt(key) != data.id) {
+					addItemInUsersFile(data, obj);
+				}
+			}
+		}
+	});
+}
+
+function addItemInUsersFile(data, obj) {
+	obj[data.id.toString()] = data.username;
+
+	jsonfile.writeFile(usersData, obj, {spaces: 2, EOL: '\r\n'}, (err) => {
+		err ? console.log(err) : null;
+	});
+}
+
 function sendResponse(userId, type) {
 	getData(API_LINK).then(
 		response => {

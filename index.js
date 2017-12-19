69668393950da4ece2a9eaefd38c0da4b9d468a7
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2017-12-19 13:56:13 +0200

    Edit js code

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
+// Modules
+// ------------------------------------------------------------
 let TelegramBot = require('node-telegram-bot-api');
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+
+// Variables
+// ------------------------------------------------------------
 const API_LINK = 'https://api.nasa.gov/planetary/apod?api_key=rom93FHJOFb6TF4jSC7USdH03jogPMtfg7qDHrMd';
 const BOT_TOKEN = '508617689:AAEuLPKs-EhrjrYGnz60inYNZqakf6HJWc0';
 const BOT = new TelegramBot(BOT_TOKEN, {polling: true});
 
-BOT.onText(/\/pic/, function (msg, match) {
-	let userId = msg.from.id;
 
-	getData(API_LINK).then(
-		response => {
-			BOT.sendPhoto(userId, JSON.parse(response).url);
-		},
-		error => console.log(error)
-	);
+// Bot response
+// ------------------------------------------------------------
+BOT.onText(/\/pic/, function (msg, match) {
+	sendResponse(msg.from.id, 'pic');
 });
 
 BOT.onText(/\/desc/, function (msg, match) {
-	let userId = msg.from.id;
-
-	getData(API_LINK).then(
-		response => {
-			let parseResponse = JSON.parse(response);
-			let desc = '<b>' + parseResponse.title + '</b>' + '\n' + parseResponse.explanation;
-
-			BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
-		},
-		error => console.log(error)
-	);
+	sendResponse(msg.from.id, 'desc');
 });
 
 BOT.onText(/\/all/, function (msg, match) {
-	let userId = msg.from.id;
-	
+	sendResponse(msg.from.id, 'all');
+});
+
+
+// Functions
+// ------------------------------------------------------------
+function sendResponse(userId, type) {
 	getData(API_LINK).then(
 		response => {
 			let parseResponse = JSON.parse(response);
 			let desc = '<b>' + parseResponse.title + '</b>' + '\n' + parseResponse.explanation;
 
-			BOT.sendPhoto(userId, parseResponse.url);
-			BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
+			switch(type) {
+				case 'pic':
+					BOT.sendPhoto(userId, parseResponse.url);
+					break;
+
+				case 'desc':
+					BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
+					break;
+
+				case 'all':
+					BOT.sendPhoto(userId, parseResponse.url);
+					BOT.sendMessage(userId, desc, {parse_mode: 'HTML'});
+					break;
+			}
 		},
 		error => console.log(error)
 	);
-});
+}
 
 function getData(url) {
     return new Promise(function(resolve, reject) {
